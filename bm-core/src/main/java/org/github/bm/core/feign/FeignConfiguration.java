60b48dbe454f12b623aad94c8cf5e28a7e1355bf
zuuuYao <17685306043@163.com>
2025-08-02 01:38:38 +0800

    feat(core): 添加 Feign拦截器并集成 OpenFeign
    - 新增 FeignConfiguration 类，实现 Feign 请求拦截功能- 在 pom.xml 中添加 spring-cloud-starter-openfeign 依赖- 在根 pom.xml 中添加 openfeign 版本号
    - 更新 spring-cloud-dependencies 以包含 OpenFeign 相关依赖

---
+++ bm-core/src/main/java/org/github/bm/core/feign/FeignConfiguration.java
@@ -0,0 +1,48 @@
+package org.github.bm.core.feign;
+
+import cn.hutool.core.util.StrUtil;
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.github.bm.common.security.SecurityConstants;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * feign拦截器
+ */
+@Slf4j
+@Configuration
+public class FeignConfiguration {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+
+    @Bean(name = "feignRequestInterceptor")
+    public RequestInterceptor feignRequestInterceptor() {
+        log.info("注册Feign拦截器...");
+        return template -> {
+            // 获取请求
+            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+
+            if (null != requestAttributes) {
+                ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
+                HttpServletRequest request = servletRequestAttributes.getRequest();
+                // 透传请求源
+                String source = request.getHeader(SecurityConstants.REQUEST_SOURCE);
+                source = StrUtil.isNotBlank(source) ? source + " -> " + appName : source;
+                template.header(SecurityConstants.REQUEST_SOURCE, source);
+                // 透传请求上下文
+                String authorizationContextHolder = request.getHeader(SecurityConstants.GATEWAY_AUTHORIZATION_CONTEXT_HOLDER_KEY);
+                if (authorizationContextHolder != null) {
+                    template.header(SecurityConstants.GATEWAY_AUTHORIZATION_CONTEXT_HOLDER_KEY, authorizationContextHolder);
+                }
+            }
+        };
+    }
+}
