7973fc649657d9f0536c76345b8c6a8d2dfa4afc
zuuuYao <17685306043@163.com>
2025-08-07 23:10:26 +0800

    refactor(bm-datareport):优化数据报告应用启动日志- 移除了未使用的导入项 - 替换了硬编码的应用名称为 ServiceEnum.APPLICATION_DATAREPORT.name - 更新了 uReport designer 访问 URL 的显示格式- 删除了冗余的网络地址信息输出

--- bm-service/bm-datareport/bm-ureport2-console/src/main/java/com/bstek/ureport/console/DataReportApplication.java
+++ bm-service/bm-datareport/bm-ureport2-console/src/main/java/com/bstek/ureport/console/DataReportApplication.java
@@ -1,6 +1,5 @@
 package com.bstek.ureport.console;
 
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.github.bm.common.enums.ServiceEnum;
 import org.github.bm.common.launch.BMApplication;
@@ -8,40 +7,28 @@ import org.github.bm.core.annotations.BMCloudApplication;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.core.env.Environment;
 
-import java.net.InetAddress;
 @Slf4j
 @ImportResource("classpath:context.xml")
 @BMCloudApplication
 public class DataReportApplication implements ApplicationRunner {
-    @Resource
-    Environment env;
     public static void main(String[] args) {
         BMApplication.run(ServiceEnum.APPLICATION_DATAREPORT.name, DataReportApplication.class, args);
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String serverPort = env.getProperty("server.port");
-        String appName = env.getProperty("spring.application.name");
-        String hostAddress = InetAddress.getLocalHost().getHostAddress();
         log.info(
                 """
                         \n
                         ----------------------------------------------------------
                             Application '{}' is running! Access URLs:
-                            Local: http://localhost:{}
-                            External: http://{}:{}
-                            uReport designer: http://{}:{}/ureport/designer
+                            uReport designer: http://{}:port/{}/ureport/designer
                         ----------------------------------------------------------
                         """,
-                appName,
-                serverPort,
-                hostAddress,
-                serverPort,
-                hostAddress,
-                serverPort
+                ServiceEnum.APPLICATION_DATAREPORT.name,
+                ServiceEnum.APPLICATION_GATEWAY.name,
+                ServiceEnum.APPLICATION_DATAREPORT.name
         );
     }
 }
