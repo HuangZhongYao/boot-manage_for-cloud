11570f51b4ddc3f331a70eaed8a42a195be8dcad
zuuuYao <15691549+melonm@user.noreply.gitee.com>
2025-08-15 00:01:57 +0800

    feat(bm-system): 添加角色管理相关接口
    - 新增 RoleController 类，实现角色管理的各个接口
    - 包含分页查询、根据状态查询、查询角色下用户、设置角色用户、添加角色、编辑角色、启用/停用角色和删除角色等功能
    - 使用 Swagger 注解进行接口文档生成- 集成 Knife4j 插件，增强接口文档展示效果

---
+++ bm-service/bm-system/src/main/java/org/github/bm/system/web/RoleController.java
@@ -0,0 +1,102 @@
+package org.github.bm.system.web;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import lombok.AllArgsConstructor;
+import org.github.bm.common.base.dto.input.BaseManyLongIdInputDTO;
+import org.github.bm.common.base.web.BaseController;
+import org.github.bm.system.dto.*;
+import org.github.bm.system.service.IRoleService;
+import org.github.bm.system.vo.RolePageQueryListItemVo;
+import org.github.bm.system.vo.RoleUserModel;
+import org.github.bm.system.vo.RoleVo;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Desc: Created by IntelliJ IDEA.
+ * @Author: ZhongYao.Huang
+ * @Copyright: ZuuuuYao By Github
+ * @Time: 2024-07-13 18:41
+ */
+@Tag(name = "角色接口")
+@AllArgsConstructor
+@RestController
+@RequestMapping("/role")
+public class RoleController extends BaseController {
+
+    @Resource
+    IRoleService roleService;
+
+    @Operation(summary = "分页查询", description = "分页查询角色接口")
+    @GetMapping(value = "/pageQueryList", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperationSupport(authors = "zuuuYao")
+    public Page<RolePageQueryListItemVo> pageQueryList(RolePageQueryInputDTO inputDTO) {
+        return roleService.pageQueryList(inputDTO);
+    }
+
+    @Operation(summary = "根据状态查询角色", description = "根据角色启用状态查询")
+    @GetMapping(value = "/queryList", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Parameters({
+        @Parameter(name = "enable", description = "true|false|不传递,true查询启用,false=查询禁用,不传入=查询全部"),
+    })
+    @ApiOperationSupport(authors = "zuuuYao")
+    public List<RoleVo> queryList(Boolean enable) {
+        return roleService.queryList(enable);
+    }
+
+    @Operation(summary = "查询角色下的用户", description = "查询指定角色下有哪些用户")
+    @GetMapping(value = "/queryRoleUserList", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Parameters({
+        @Parameter(name = "id", description = "角色id"),
+    })
+    @ApiOperationSupport(authors = "zuuuYao")
+    public List<RoleUserModel> queryRoleUserList(@RequestParam(name = "id") Long id) {
+        return roleService.queryRoleUserList(id);
+    }
+
+    @Operation(summary = "角色设置用户", description = "批量给角色设置用户")
+    @ApiResponse(responseCode = "200", description = "ok")
+    @PostMapping(value = "/setRoleUser", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Boolean setRoleUser(@RequestBody @Validated SetRoleUserInputDTO inputDTO) {
+        return roleService.setRoleUser(inputDTO);
+    }
+
+    @Operation(summary = "添加角色")
+    @ApiResponse(responseCode = "200", description = "ok")
+    @PostMapping(value = "/addRole",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Boolean addRole(@RequestBody @Validated AddRoleInputDTO inputDTO) {
+        return roleService.addRole(inputDTO);
+    }
+
+    @Operation(summary = "编辑角色")
+    @ApiResponse(responseCode = "200", description = "ok")
+    @PatchMapping(value = "/editRole",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Boolean editRole(@RequestBody @Validated EditRoleInputDTO inputDTO) {
+        return roleService.editRole(inputDTO);
+    }
+
+    @Operation(summary = "启用|停用角色", description = "启用|停用角色接口")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @PatchMapping(value = "/setState", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Boolean setState(@RequestBody @Validated SetRoleStateInputDTO inputDTO) {
+        return roleService.setState(inputDTO);
+    }
+
+    @Operation(summary = "删除角色")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @DeleteMapping(value = "/delRole", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Boolean delRole(@RequestBody @Validated BaseManyLongIdInputDTO inputDTO) {
+        return roleService.delRole(inputDTO);
+    }
+
+}
