5ed71e4b8b9b9fffa8ffc8f51f94c03423cc3bf5
zuuuYao <17685306043@163.com>
2025-08-25 15:56:47 +0800

    build(web): 将前端代码单独使用仓库

--- web/src/views/develop/datasource/api.js
+++
@@ -1,8 +0,0 @@
-import { request } from '@/utils'
-
-export default {
-  create: data => request.post('/bm-resource/dataSource/addDataSource', data),
-  read: (params = {}) => request.get('/bm-resource/dataSource/pageQueryList', { params }),
-  update: data => request.patch(`/bm-resource/dataSource/editDataSource`, data),
-  delete: data => request.delete(`/bm-resource/dataSource/delDataSource`, { data }),
-}
