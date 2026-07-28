e3a0c01a2107d87d96eef506efd8caa4c3d72785
黄中瑶 <17685306043@163.com>
2026-07-28 16:06:24 +0800

    refactor(pom): 合并common模块

--- bm-common/bm-core/src/main/java/org/github/bm/core/mybatis/MybatisPlusConfiguration.java
+++ bm-common/bm-core/src/main/java/org/github/bm/core/mybatis/MybatisPlusConfiguration.java
@@ -2,9 +2,8 @@ package org.github.bm.core.mybatis;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.IllegalSQLInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-//import org.mybatis.spring.annotation.MapperScan;
+import org.github.bm.common.constant.AppConstant;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +19,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @Time 2024-07-11 16:22
  * @Author HuangZhongYao
  */
-@MapperScan("org.github.bm.**.repository")
+@MapperScan(AppConstant.BASE_PACKAGES + ".**.repository")
 @Configuration
 @EnableTransactionManagement
 public class MybatisPlusConfiguration {
